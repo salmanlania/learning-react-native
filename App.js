bd05dce1c46f855cdfa10156671890e7484d27e4
indenta-ai <shahbazabdullah81@gmail.com>
2024-07-25 09:52:38 +0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.Container}>
+      <Text>Welcome To First React Native App</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  Container: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
   },
-});
+})
---
+++ AppStyles.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native'
+
+const Appstyles = StyleSheet.create({
+    Container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 16,
+        alignItems: 'center',
+        width: '100%'
+    }
+})
+
+export default Appstyles;
